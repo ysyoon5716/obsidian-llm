77190179665d477e738af6dbbd46d337bcd45ee5
ysyoon <ysyoon5716@gmail.com>
2025-08-16 11:44:39 +0900

    Initial commit

--- main.ts
+++ main.ts
@@ -1,85 +1,109 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FuzzySuggestModal, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Vault } from 'obsidian';
+import OpenAI from 'openai';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface LLMSettings {
+	apiKey: string;
+	promptFolder: string;
+	modelName: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: LLMSettings = {
+	apiKey: '',
+	promptFolder: '_prompt',
+	modelName: 'gpt-5-nano',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+export class PromptModal extends FuzzySuggestModal<string> {
+	plugin: LLMPlugin;
+	
+	constructor(app: App, plugin: LLMPlugin) {
+		super(app);
+		this.plugin = plugin;
 	}
 
-	onunload() {
+	getItems(): string[] {
+		const promptFolder = this.plugin.app.vault.getFolderByPath(this.plugin.settings.promptFolder);
+		if (!promptFolder) {
+			new Notice('Prompt folder not found');
+			return [];
+		}
+		
+		const prompts: string[] = [];
+		Vault.recurseChildren(promptFolder, (file) => {
+			if (file.name.endsWith('.md')) {
+				prompts.push(file.name.replace('.md', ''));
+			}
+		});
+		console.log('prompts', prompts);
+		return prompts;
+	}
 
+	getItemText(item: string): string {
+		return item;
+	}
+
+	async onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): Promise<void> {
+		console.log('chosen item', item);
+		const promptFile = this.plugin.app.vault.getFileByPath(`${this.plugin.settings.promptFolder}/${item}.md`);
+		if (!promptFile) {
+			new Notice('Prompt file not found');
+			return;
+		}
+		let prompt = await this.plugin.app.vault.read(promptFile);
+
+		const activeFile = this.app.workspace.getActiveFile();
+		const title = activeFile?.basename || '';
+		console.log('title', title);
+
+		prompt = prompt.replace('{title}', title);
+		console.log('prompt', prompt);
+
+		const client = new OpenAI({
+			apiKey: this.plugin.settings.apiKey,
+			dangerouslyAllowBrowser: true,
+		});
+		
+
+		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor!;
+		const cursor = editor.getCursor();
+		let from = {line: cursor.line, ch: cursor.ch};
+		console.log(cursor);
+
+		const stream = await client.responses.create({
+			model: this.plugin.settings.modelName,
+			input: prompt,
+			stream: true,
+		});
+
+		for await (const event of stream) {
+			if (event.type === 'response.output_text.delta') {
+				editor.replaceRange(event.delta, from);
+				from.ch += event.delta.length;
+			}	
+		}
+	}
+}
+
+export default class LLMPlugin extends Plugin {
+	settings: LLMSettings;
+
+	async onload() {
+		console.log('loading LLM plugin');
+		await this.loadSettings();
+		console.log('loaded settings');
+		this.addSettingTab(new LLMSettingTab(this.app, this));
+
+		this.addCommand({
+			id: 'generate-text',
+			name: 'Generate Text',
+			callback: () => {
+				console.log('generating text');
+				const modal = new PromptModal(this.app, this);
+				modal.open();
+			}
+		});
 	}
 
 	async loadSettings() {
@@ -88,46 +112,42 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		console.log('saved settings');
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class LLMSettingTab extends PluginSettingTab {
+	plugin: LLMPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LLMPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	display(): void {
+	display() {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('OPENAI_API_KEY')
+			.setDesc('The API key to use for the LLM')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Model Name')
+			.setDesc('The model to use for the LLM')
+			.addText(text => text
+				.setPlaceholder('Enter the model name')
+				.setValue(this.plugin.settings.modelName)
+				.onChange(async (value) => {
+					this.plugin.settings.modelName = value;
 					await this.plugin.saveSettings();
 				}));
 	}
